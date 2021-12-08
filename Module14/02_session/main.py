f2c79553386a572f82b970402969696174a86507
Wolfich <volchenok8888@mail.ru>
2021-12-08 15:00:01 +0300

    сделанно

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def the_equation_calculate_output():
+    print("Введите первую точку")
+    x1 = float(input('X: '))
+    y1 = float(input('Y: '))
+    print("\nВведите вторую точку")
+    x2 = float(input('X: '))
+    y2 = float(input('Y: '))
+
+    x_diff = x1 - x2
+    y_diff = y1 - y2
+    if x_diff != 0:
+        k = y_diff / x_diff
+        b = y2 - k * x2
+
+        print("Уравнение прямой, проходящей через эти точки:")
+        print("y = ", k, " * x + ", b)
+    else:
+        print('Знаменатель равен нулю,на ноль делить нельзя, введите новые значения.\n')
+        the_equation_calculate_output()
+
+
+the_equation_calculate_output()
