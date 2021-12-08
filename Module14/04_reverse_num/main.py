ad390b9b25729e37f2f43e0e40fef1bc66cfad17
Wolfich <volchenok8888@mail.ru>
2021-12-08 19:37:48 +0300

    сделанно

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,26 @@
-# TODO здесь писать код
+def revers_number_calculate(number):           #функция реверса числа
+    exp_number_1 = ''
+    exp_number_2 = ''
+    for symbol in number:                       #цикл который переберает символы в строке до точки
+        exp_number_2 += symbol
+        if symbol == '.':
+            exp_number_1 = exp_number_2
+            exp_number_2 = ''
+            exp_number_2 += symbol
+    exp_number_1 = (exp_number_1[::-1])         #переворачиваем строки
+    exp_number_2 = (exp_number_2[::-1])
+
+    number = (exp_number_1.replace('.', '') + '.' + exp_number_2.replace('.', '')) #создаем правильное число в строке
+    return (float(number))      #переводим строку в вещественное число
+
+number_N = input('Введите число N: ')
+number_K = input('Введите число K: ')
 
 
+reverse_number_N = revers_number_calculate(number_N)
+reverse_number_K = revers_number_calculate(number_K)
+result = reverse_number_N + reverse_number_K
+print('Первое число наборот: ', reverse_number_N)
+print('Второе число наборот: ', reverse_number_K)
+
+print('Сумма: ', result)
