717559a756a02bb2c17cae0b7668b992f18dd489
Wolfich <volchenok8888@mail.ru>
2021-12-08 15:30:49 +0300

    сделанно

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+def sum_of_digits_calculate():
+    number = int(input('Введите число: '))
+    summ = 0
+    while number != 0:
+        result = number % 10
+        summ += result
+        number //= 10
+    return summ
+
+
+def amount_calculate():
+    number = int(input('Введите число: '))
+    count = 0
+    while number != 0:
+        count += 1
+        number //= 10
+    return count
+
+difference = (sum_of_digits_calculate()) - (amount_calculate())
+print('Разность суммы чисел и количества равно: ', difference)
+
