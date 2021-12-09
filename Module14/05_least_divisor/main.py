22d030e7d3e373124199119bff94057266b0ab92
Wolfich <volchenok8888@mail.ru>
2021-12-09 17:20:30 +0300

    сделанно

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+number = int(input('Введите число:  '))
+coefficient = 1
+while coefficient <= number:
+    coefficient = coefficient + 1
+    if number % coefficient == 0:
+        print(coefficient)
+        break
+
