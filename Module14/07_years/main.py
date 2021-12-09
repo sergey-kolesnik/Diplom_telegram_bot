37946759f4d63c67a434fba0051b36ff57089ef0
Wolfich <volchenok8888@mail.ru>
2021-12-09 18:43:22 +0300

    сделанно

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def year_calculate_output(start, stop):
+    for year in range(start, stop + 1):
+        for check in range(10):
+            count = 0
+            for number in str(year):
+                if int(number) == int(check):
+                    count += 1
+                if count >= 3:
+                    print(year)
+
+
+start_year = int(input('Введите первый год: '))
+stop_year = int(input('Введите последний год: '))
+print(f'Года от {start_year} до {stop_year} c тремя одинаковыми цифрами :')
+
+
+year_calculate_output(start_year, stop_year)
