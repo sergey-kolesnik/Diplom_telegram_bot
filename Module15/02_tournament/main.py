e7f1a604eecee19fb7156859b80be02eb35ee5a5
сергей колесник <volchenok8888@mail.ru>
2021-12-17 17:33:02 +0300

    сделано

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+name_list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+one_day_name = []
+
+for index in range(1, len(name_list) + 1, 2 ):
+    one_day_name.append(name_list[index])
+print('Первый день: ',one_day_name)
