52643c712c9703bc0d58de00a310c983cab9e878
сергей колесник <volchenok8888@mail.ru>
2021-12-17 17:10:15 +0300

    сделано

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,8 +1,8 @@
 number = int(input('Введите число: '))
 number_list = []
 
-for i in range(1, number + 1, 2):
-    number_list.append(i)
+for check in range(1, number + 1, 2):
+    number_list.append(check)
 
 
-print(number_list)
+print('Список нечетных чисел от 1 до', number,  ':', number_list)
