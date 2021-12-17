1a08bc39aa0784d1a2e87254b1a89d3aecde033c
сергей колесник <volchenok8888@mail.ru>
2021-12-17 18:25:06 +0300

    сделано

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+quantity_cell = int(input('Введите количество клеток: '))
+
+effectiveness_List = []
+number = 1
+index = 0
+for _ in range(quantity_cell):
+    print('Эффективность', number, 'клетки: ' ,end = '')
+    cell = int(input(''))
+    number += 1
+    index += 1
+    if index > cell:
+        effectiveness_List.append(cell)
+
+print('Неэффективные значения: ', end = '')
+for i in effectiveness_List:
+    print(i, end = ' ')
+
