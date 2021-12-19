7740eea3a0b0e0736a01e5ee4b92b312215491dd
сергей колесник <volchenok8888@mail.ru>
2021-12-19 13:15:42 +0300

    сделано

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,20 @@
-# TODO здесь писать код
+video_list = []
+quantity = int(input('ведите колличество видео карт: '))
+number = 1
 
 
+
+for _ in range(quantity):
+    print(number, 'видео карта:', end =' ')
+    model = int(input(''))
+    video_list.append(model)
+    number += 1
+
+print('Старый список видео карт: ', video_list)
+
+buy = int(input('Сколько моделей купили: '))
+
+for _ in range(buy):
+    max_model = max(video_list)
+    video_list.remove(max_model)
+print('Новый список список видео карт: ', video_list)
