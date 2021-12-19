60c5332aa2b7aa33a0def9f973237da892d6fb30
сергей колесник <volchenok8888@mail.ru>
2021-12-19 17:54:47 +0300

    сделано

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+word_list = list(word)
+rev_list = word_list[::-1]
+
+
+if word_list == rev_list:
+    print('Слово является палиндромом')
+else:
+    print('Слово палиндромом не является')
