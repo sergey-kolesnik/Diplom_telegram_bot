08ee23606203204d57714bc603054d0693342fe4
сергей колесник <volchenok8888@mail.ru>
2021-12-19 17:30:42 +0300

    сделано

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+text_list = input('ведите бегущую строку: ')
+shift = int(input('Сдвиг: '))
+print('Изначальный текст: ', text_list)
+
+text_list = text_list[shift:]+text_list[:shift]
+print('Сдвинутый текст: ', text_list)
