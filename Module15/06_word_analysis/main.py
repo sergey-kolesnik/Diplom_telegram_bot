ae7a59e8ccecd23f53dfddd5f6d982b07b284617
сергей колесник <volchenok8888@mail.ru>
2021-12-19 15:53:22 +0300

    сделано

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def unique_symbol_output(word_function):
+    word_list = list(word_function)
+    list_count = []
+    count = 0
+    for i in word_list:
+        if i not in list_count:
+            list_count.append(i)
+            count += 1
+
+    print('Уникальных символов:', count)
+
+
+while True:
+    word = input('Введите слово: ')
+    unique_symbol_output(word)
+
+
