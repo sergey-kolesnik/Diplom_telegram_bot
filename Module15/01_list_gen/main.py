7ca0cf69ed87f75169cfbc9454473e62834f6ac8
сергей колесник <volchenok8888@mail.ru>
2021-12-20 13:00:05 +0300

    сделано

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,10 +1,9 @@
 number = int(input('Введите число: '))
-number_list = []
+numbers = []
 
 for check in range(1, number + 1, 2):
-    number_list.append(check)
+    numbers.append(check)
 
-
-print('Список нечетных чисел от 1 до', number,  ':', number_list)
+print('Список нечетных чисел от 1 до', number,  ':', numbers)
 
 
--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,21 +1,17 @@
 quantity_cell = int(input('Введите количество клеток: '))
 
-effectiveness_List = []
+effectiveness = []
 number = 1
 index = 0
 for _ in range(quantity_cell):
-    print('Эффективность', number, 'клетки: ' ,end = '')
-    # TODO пустых инпутов быть не должно
-    cell = int(input(''))
+    cell = int(input(f'Эффективность {number} клетки: '))
     number += 1
     index += 1
     if index > cell:
-        effectiveness_List.append(cell)
+        effectiveness.append(cell)
 
-print('Неэффективные значения: ', end = '')
-for i in effectiveness_List:
-    print(i, end = ' ')
+print('Неэффективные значения: ', end='')
+for text in effectiveness:
+    print(text, end=' ')
 
-# TODO не должно быть подчеркиваний
-# TODO однобуквенные переменные называть развернуто
 
