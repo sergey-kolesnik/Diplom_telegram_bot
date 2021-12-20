1da88d8268338118223a2517e8cbf7d3dac5284b
сергей колесник <volchenok8888@mail.ru>
2021-12-20 13:23:12 +0300

    сделано

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,22 +1,18 @@
-video_list = []
+video = []
 quantity = int(input('ведите колличество видео карт: '))
 number = 1
 
 
-
 for _ in range(quantity):
-    print(number, 'видео карта:', end =' ')
-    model = int(input(''))
-    video_list.append(model)
+    model = int(input(f'{number} видео карта: '))
+    video.append(model)
     number += 1
 
-print('Старый список видео карт: ', video_list)
-
+print('Старый список видео карт: ', video)
 buy = int(input('Сколько моделей купили: '))
 
 for _ in range(buy):
-    max_model = max(video_list)
-    video_list.remove(max_model)
-print('Новый список список видео карт: ', video_list)
+    max_model = max(video)
+    video.remove(max_model)
+print('Новый список список видео карт: ', video)
 
-# TODO применить рекомендации от 03 задания
