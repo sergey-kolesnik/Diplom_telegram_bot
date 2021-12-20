a0863902198338fd11413ce0d972ee1666d602c7
сергей колесник <volchenok8888@mail.ru>
2021-12-20 13:04:25 +0300

    сделано

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,16 +1,12 @@
 
-number_list = [1, 4, -3, 0, 10]
-print('Изначальный список:', number_list)
-
-count = len(number_list)                            #число элементов в списке
+numbers = [1, 4, -3, 0, 10]
+print('Изначальный список:', numbers)
+count = len(numbers)                            #число элементов в списке
 
 for num_i in range(0, count - 1):       #цикл с count - 1 итераций
     for num_j in range(0, count - 1 - num_i):   #цикл с count - 1 итераций минус последнее число
-        if number_list[num_j] > number_list[num_j + 1]:  #сравниваем 2 последних числа, если a>b, то меняем их местами
-            number_list[num_j], number_list[num_j + 1] = number_list[num_j + 1], number_list[num_j]
+        if numbers[num_j] > numbers[num_j + 1]:  #сравниваем 2 последних числа, если a>b, то меняем их местами
+            numbers[num_j], numbers[num_j + 1] = numbers[num_j + 1], numbers[num_j]
 
 
-print('Отсортированный список:', number_list)
-
-# TODO применить рекомендации от 03 задания
-# TODO применить рекомендации от 03 модуля
+print('Отсортированный список:', numbers)
