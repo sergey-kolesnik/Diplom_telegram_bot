d3d26e0d1afd999b295f10ecef9f7c07f1764561
Wolfich <volchenok8888@mail.ru>
2022-05-09 20:35:09 +0300

    сделано

--- database/database.py
+++ database/database.py
@@ -2,32 +2,34 @@ import json
 from states.class_User import User
 import datetime
 from loader import bot
-from telebot.types import  CallbackQuery
+from telebot.types import CallbackQuery
+import os
 
-def recording_history_in_json(id) -> None:
+
+def recording_history_in_json(user_id) -> None:
     """Которая открывает базу данных(JSON),
     проверяет есть ли в базе ID юзера,
     если нет то добавляет в базу,
      если есть то дописывает его историю поиска
-     :param id: int
+     :param user_id: int
      :return: None"""
-    user = User.get_user(id)
-    # TODO в данном месте она не используется вы ее объявляете ниже
-    work_data = {}
+    user = User.get_user(user_id)
+
     with open('data_json.json', 'r') as file:
         work_data = json.load(file)
-        print(work_data)
-    if str(id) in work_data.keys():
+
+    if str(user_id) in work_data.keys():
         date_time = datetime.datetime.now()
         key_data_time = date_time.strftime('%d-%m-%Y %H:%M:%S')
-        work_data[str(id)].append({key_data_time: {'mode': user.mode,
-                                              'hotel': [hotel['name'] for hotel in user.total_data_hotel]}})
+        work_data[str(user_id)].append({key_data_time: {'mode': user.mode,
+                                                        'hotel': [hotel['name'] for hotel in user.total_data_hotel]}})
 
         with open('data_json.json', 'w') as file:
             json.dump(work_data, file, indent=4)
     else:
-        work_data[id] = [{str(datetime.datetime.now()): {'mode': user.mode,
-                                                'hotel': [hotel['name'] for hotel in user.total_data_hotel]}}]
+        work_data[user_id] = [{str(datetime.datetime.now()): {'mode': user.mode,
+                                                              'hotel': [hotel['name'] for hotel in
+                                                                        user.total_data_hotel]}}]
         # TODO этот кусок кода явно повторяется его можно вынести в функцию
         with open('data_json.json', 'w') as file:
             json.dump(work_data, file, indent=4)
@@ -38,9 +40,8 @@ def output_history_in_json(call: CallbackQuery) -> None:
      а так же для вывода информации юзеру.
      :param call: CallbackQuery
      :return: None"""
-    # TODO код сломается у меня он не запуститься
-    # TODO пути должны быть относительные + \\ заменить на / или использовать метод который сам подставит нужные
-    with open('C:\\Users\\Wolfich\\PycharmProjects\\Diplom_telegram_bot\\data_json.json', 'r') as file:
+    path_data_history = os.path.abspath('data_json.json')
+    with open(path_data_history, 'r') as file:
         data_history = json.load(file)
     if str(call.from_user.id) in data_history:
 
