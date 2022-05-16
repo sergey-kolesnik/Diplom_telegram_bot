30c6837e5597477a1c40d991abf22b2a7e844e45
Сергей Колесник <volchenok8888@mail.ru>
2022-05-16 12:15:59 +0000

    Delete main.py

--- Module15/02_tournament/main.py
+++
@@ -1,8 +0,0 @@
-names = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
-one_day_name = []
-
-
-for index, name in enumerate(names):
-    if index % 2 == 0:
-        one_day_name.append(name)
-print('Первый день: ', one_day_name, end=' ')
