d3bd9b4c9b10b0f343ff3f65d6a372b504209599
Сергей Колесник <volchenok8888@mail.ru>
2022-05-16 12:17:11 +0000

    Delete main.py

--- Module15/08_running_nums/main.py
+++
@@ -1,8 +0,0 @@
-texts = input('ведите бегущую строку: ')
-shift = int(input('Сдвиг: '))
-print('Изначальный текст: ', texts)
-
-texts = texts[shift:]+texts[:shift]
-print('Сдвинутый текст: ', texts)
-
-
