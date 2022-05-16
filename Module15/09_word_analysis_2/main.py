578ef8481a2d4bba32ed32e1a2d26493bb11165c
Сергей Колесник <volchenok8888@mail.ru>
2022-05-16 12:17:25 +0000

    Delete main.py

--- Module15/09_word_analysis_2/main.py
+++
@@ -1,8 +0,0 @@
-word = input('Введите слово: ')
-words = list(word)
-rev_word = words[::-1]
-
-if words == rev_word:
-    print('Слово является палиндромом')
-else:
-    print('Слово палиндромом не является')
