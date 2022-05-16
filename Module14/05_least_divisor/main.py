51be29028101b6353243c24b1f2c4dbf97e6fb9c
Сергей Колесник <volchenok8888@mail.ru>
2022-05-16 12:15:02 +0000

    Delete main.py

--- Module14/05_least_divisor/main.py
+++
@@ -1,8 +0,0 @@
-number = int(input('Введите число:  '))
-coefficient = 1
-while coefficient <= number:
-    coefficient = coefficient + 1
-    if number % coefficient == 0:
-        print(coefficient)
-        break
-
